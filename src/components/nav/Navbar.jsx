cc57b072dc910d8fba7bee2059ebff793e4a52c2
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-03-06 19:16:57 +0100

    global style-flex

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <a to="/">
+        <i>{"Feyza"}</i>
+        <span>Recipe</span>
+      </a>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="about">about</Link>
+        <Link to="register">Register</Link>
+        <Link to="logout">Logout</Link>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
