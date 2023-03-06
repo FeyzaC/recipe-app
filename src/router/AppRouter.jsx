c9b512ef991e8e19ea23827bba38f976b9efe30b
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-03-06 18:54:26 +0100

    AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+
 const AppRouter = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
